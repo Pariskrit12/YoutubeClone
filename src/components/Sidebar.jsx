9cc829e5c05a13822a8832bb5f35a114b3ae4def
Pariskrit12 <bhagataryan784@gmail.com>
2025-05-25 23:19:41 +0545

    Added Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse, faClockRotateLeft, faThumbsUp, faVideo, faBell, faGear } from '@fortawesome/free-solid-svg-icons';
+
+const SidebarItem = ({ icon, label, active = false }) => (
+  <div
+    className={`flex flex-col items-center p-3 rounded-xl cursor-pointer transition-all ${
+      active ? 'bg-gray-700 text-white' : 'hover:bg-gray-300'
+    }`}
+  >
+    <FontAwesomeIcon icon={icon} className="text-lg mb-1" />
+    <span className="text-xs font-medium">{label}</span>
+  </div>
+);
 
 export default function Sidebar() {
   return (
-    <div className=' bg-gray-200 w-[16rem] '>
-      
+    <div className="hidden lg:flex bg-white text-black w-24 h-screen  flex-col items-center pt-4 space-y-4">
+      <SidebarItem icon={faHouse} label="Home" active />
+      <SidebarItem icon={faClockRotateLeft} label="History" />
+      <SidebarItem icon={faThumbsUp} label="Liked" />
+      <SidebarItem icon={faVideo} label="Videos" />
+      <SidebarItem icon={faBell} label="Subs" />
+      <SidebarItem icon={faGear} label="Settings" />
     </div>
-  )
+  );
 }

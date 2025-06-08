0af29e48c9dc1064a634adb5f88747b4b801f7a8
Pariskrit12 <bhagataryan784@gmail.com>
2025-06-08 23:42:25 +0545

    Fetched the data from the server in the home page

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,21 +1,21 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import VideoCard from "../components/VideoCard";
-
+import axios from "axios";
 export default function Home() {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    const fetchAllVideos = async () => {
+      const response = await axios.get("/api/v1/videos/get-all-video");
+      console.log(response.data.data);
+      setVideos(response.data.data);
+    };
+    fetchAllVideos();
+  },[]);
   return (
-    <div className=" xl:ml-[1rem] grid grid-cols-2 lg:grid-cols-3 ">
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
+    <div className=" xl:ml-[1rem] grid grid-cols-2 md:grid-cols-3 ">
+      {videos.map((video)=>(
+        <VideoCard video={video} key={video._id}/>
+      ))}
     </div>
   );
 }

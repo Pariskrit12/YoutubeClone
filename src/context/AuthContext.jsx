a7eabdde55d8f76c4330a89eafe07ab3d4dcecfc
Pariskrit12 <bhagataryan784@gmail.com>
2025-06-10 00:42:23 +0545

    Implement persistent login using AuthContext and HTTP-only cookies

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const isLoggedIn = async () => {
+      try {
+        const response = await axios.get("/api/v1/users/get-personal-info", {
+          withCredentials: true,
+        });
+        setUser(response.data.data);
+      } catch (error) {
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+  }, []);
+
+  const login = async (email, password) => {
+    const response = await axios.post(
+      "/api/v1/users/login",
+      { email, password },
+      { withCredentials: true }
+    );
+    setUser(response.data.data);
+  };
+
+  const logout = async () => {
+    await axios.post("/api/v1/users/logout", {}, { withCredentials: true });
+    setUser(null);
+  };
+  return (
+    <AuthContext.Provider value={{ user, login, logout, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
